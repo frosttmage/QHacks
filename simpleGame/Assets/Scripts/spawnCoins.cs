05d883896175a052bfc35fb04055cf4806157875
duko栖泽 <dukoqize@dukoqizemacbook.local>
2018-02-03 16:48:47 -0500

    count count

--- simpleGame/Assets/Scripts/spawnCoins.cs
+++ simpleGame/Assets/Scripts/spawnCoins.cs
@@ -14,16 +14,16 @@ public class spawnCoins : MonoBehaviour {
 
 
 	void Start () {
-		lightHorizontalDis [0] = 12f;
-		lightHorizontalDis [1] = 12f;
-		lightHorizontalDis [2] = 12f;
-		lightHorizontalDis [3] = 12f;
-		lightHorizontalDis [4] = 12f;
-		cubeHorizontalDis [0] = 12f;
-		cubeHorizontalDis [1] = 12f;
-		cubeHorizontalDis [2] = 12f;
-		cubeHorizontalDis [3] = 12f;
-		cubeHorizontalDis [4] = 12f;
+		lightHorizontalDis [0] = 6f;
+		lightHorizontalDis [1] = 6f;
+		lightHorizontalDis [2] = 6f;
+		lightHorizontalDis [3] = 6f;
+		lightHorizontalDis [4] = 6f;
+		cubeHorizontalDis [0] = 6f;
+		cubeHorizontalDis [1] = 6f;
+		cubeHorizontalDis [2] = 6f;
+		cubeHorizontalDis [3] = 6f;
+		cubeHorizontalDis [4] = 6f;
 
 		originPosition = transform.position;
 		Spawn ();
@@ -32,38 +32,20 @@ public class spawnCoins : MonoBehaviour {
 	void Spawn()
 	{
 		for (int i = 0; i < lightHorizontalDis.Length; i++) {
-			/*
-			int coinFlip = Random.Range (0, 2);
-			int change = Random.Range (0, 2);
-			verticalDis [0] = 0f;
-			if (down) {
-				verticalDis [1] = 6f;
-			}
-			else {
-				verticalDis [1] = -6f;
-			}
-			*/
 			Vector2 randomPosition = originPosition + new Vector2 (lightHorizontalDis [i], 6f);
-			//if (coinFlip > 0) {
 			Instantiate (light, randomPosition, Quaternion.identity);
 			originPosition = randomPosition + new Vector2 (0, -6f);
-			/*
-			if (change == 1) {
-				down = !down;
-			}
-			*/
-			/*
-			}
-			else {
-				originPosition = originPosition + new Vector2(horizontalDis[i], 0f);
-			}
-		*/
+			Vector2 randomPosition2 = originPosition + new Vector2 (cubeHorizontalDis[i], 0f);
+			Instantiate (enemy, randomPosition2, Quaternion.identity);
+			originPosition = randomPosition2;
 		}
+		/*
 		for (int i = 0; i < cubeHorizontalDis.Length; i++) {
 			Vector2 randomPosition = originPosition + new Vector2 (cubeHorizontalDis[i], 0f);
 			Instantiate (enemy, randomPosition, Quaternion.identity);
 			originPosition = randomPosition;
 		}
+		*/
 	}
 
 }
